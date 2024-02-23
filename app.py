3065ed88fda1a29f6fd12e7afb90a4a25d7aecad
Aniketsu <155738622+Aniketsu@users.noreply.github.com>
2024-02-23 13:25:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import keyboard
 from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
 from audio_recorder_streamlit import audio_recorder
 from streamlit_float import *
